ef6e173db7921241a62eef0bb4bba7e59b51f54c
elvezpablo <prangel@gmail.com>
2023-01-15 15:36:21 -0800

    2 files modified

--- App.tsx
+++ App.tsx
@@ -13,8 +13,11 @@ import TreeMap from './components/TreeMap';
 import { TreeNode } from './tree/TreeNode';
 
 export default function App() {
+  const [tree, setTree] = React.useState<TreeNode | undefined>();
+
   React.useEffect(() => {
     let root = buildTree(data[0], 0, undefined, undefined);
+    setTree(root);
     // calculateInitialValues(root);
     // applyModifier(root, 0);
     // updateXVals(root);
@@ -23,7 +26,7 @@ export default function App() {
 
   return (
     <div>
-      <TreeMap  />
+      <TreeMap nodes={tree} />
       {/* <h2>Data Structure</h2>
       <pre>{JSON.stringify(data, null, 2)}</pre> */}
     </div>
--- components/TreeMap.tsx
+++ components/TreeMap.tsx
@@ -1,26 +1,22 @@
 import * as React from 'react';
-import TreeNode from '../tree/TreeNode';
+import { TreeNode } from '../tree/TreeNode';
 import CharacterNode from './CharacterNode';
 
 type Props = {
-  nodes: TreeNode[]
-}
+  nodes: TreeNode;
+};
 
 const size = {
   width: 800,
   height: 500,
 };
 
-export default function TreeMap() {
-  
-        <svg width={size.width} height={size.height}>
-          <rect
-            fill="rgb(33,33,33,.3)"
-            width={size.width}
-            height={size.height}
-          />
+export default function TreeMap({ nodes }: Props) {
+  return (
+    <svg width={size.width} height={size.height}>
+      <rect fill="rgb(33,33,33,.3)" width={size.width} height={size.height} />
 
-          <CharacterNode value="0" x={10} y={10} />
-        </svg>
-      
+      <CharacterNode value="0" x={10} y={10} />
+    </svg>
+  );
 }

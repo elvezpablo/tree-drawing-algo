f7ede4dd22bacc49cf28c408ac759209366e832d
elvezpablo <prangel@gmail.com>
2023-01-16 09:30:05 -0800

    1 file modified

--- components/TreeMap.tsx
+++ components/TreeMap.tsx
@@ -13,11 +13,12 @@ const size = {
 
 export default function TreeMap({ nodes }: Props) {
   console.log('nodes: ', nodes);
+  const rowHeight = CharacterNode.defaultValues.height + 10;
   return (
     <svg width={size.width} height={size.height}>
       <rect fill="rgb(33,33,33,.3)" width={size.width} height={size.height} />
       {nodes.map((n) => {
-        return <CharacterNode value={n.value} x={10} y={10} />;
+        return <CharacterNode value={n.value} x={10} y={n.y * rowHeight} />;
       })}
     </svg>
   );

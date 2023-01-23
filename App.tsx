ff75dcfd4a0cc110410c95a5b9e5c3132e6282c1
elvezpablo <prangel@gmail.com>
2023-01-23 07:05:58 -0800

    1 file modified

--- App.tsx
+++ App.tsx
@@ -13,26 +13,34 @@ import TreeMap from './components/TreeMap';
 import { TreeNode } from './tree/TreeNode';
 import ControlPanel from './components/ControlPanel';
 
+const getTreeData = () => {
+  let root = buildTree(data[0], 0, undefined, undefined);
+  let nodes: TreeNode[] = [root];
+  const flatTree: TreeNode[] = [];
+  while (nodes.length) {
+    let node = nodes.shift();
+    nodes = [...nodes, ...node.children];
+    flatTree.push(node);
+  }
+  return flatTree;
+};
+
 export default function App() {
   const [tree, setTree] = React.useState<TreeNode[]>([]);
-
+  const [change, setChange] = React.useState<string | undefined>();
   React.useEffect(() => {
-    let root = buildTree(data[0], 0, undefined, undefined);
-    let nodes: TreeNode[] = [root];
-    const flatTree: TreeNode[] = [];
-    while (nodes.length) {
-      let node = nodes.shift();
-      nodes = [...nodes, ...node.children];
-      flatTree.push(node);
-    }
-    setTree(flatTree);
+    const t = getTreeData();
+    setTree(t);
     // calculateInitialValues(root) ;
     // applyModifier(root, 0);
     // updateXVals(root);
   }, []);
 
+  React.useEffect(() => {}, [change]);
+
   const handleChange = (change: string, value?: number) => {
     console.log('change: ', change);
+    setChange(change);
   };
 
   return (
